dfe8884774dbd41f2b64da197f4c8811230d24c1
FRoura <fxwfran@gmail.com>
2017-01-13 18:13:46 -0300

    Better interface

---
+++ Swaggy/MahApps.Metro.Application2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using WindowsFormsApp1;
+
+namespace MahApps.Metro.Application2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            int stats = int.Parse(textBox1.Text);
+            int pid = int.Parse(textBox2.Text);
+            Int32 addr = Convert.ToInt32(textBox3.Text, 16);
+            MemoryRead.Hack(stats, pid, addr);
+        }
+    }
+}
---
+++ Swaggy/MahApps.Metro.Application2/MemRead.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    public class MemoryRead
+    {
+        const int PROCESS_WM_READ = 0x0010;
+        const int PROCESS_VM_WRITE = 0x0020;
+
+        
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess,
+          long lpBaseAddress, byte[] lpBuffer, UIntPtr dwSize, ref UIntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] buffer, int size, int lpNumberOfBytesWritten);
+
+        public static void Hack(int status, int pid, Int32 address)
+        {
+            var processHandle = OpenProcess(0x0020FFF, false, pid);
+            int dec = status * 8;
+            byte[] bytes = BitConverter.GetBytes(dec);
+
+            for (int i = address; i < address + 64; i += 8)
+            {
+                WriteProcessMemory((int)processHandle, i, bytes, (int)bytes.Length, 0);
+            }
+        }
+    }
+}
