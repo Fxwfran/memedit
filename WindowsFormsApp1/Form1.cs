89c097aa102a913ba4827bfc50fc9eccd6661f87
FRoura <fxwfran@gmail.com>
2017-01-13 17:04:40 -0300

    initial push

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int stats = int.Parse(textBox1.Text);
+            int pid = int.Parse(textBox2.Text);
+            Int32 addr = Convert.ToInt32(textBox3.Text, 16);
+            MemoryRead.Hack(stats, pid, addr);
+        }
+    }
+}
---
+++ WindowsFormsApp1/MemRead.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class MemoryRead
+    {
+        const int PROCESS_WM_READ = 0x0010;
+        const int PROCESS_VM_WRITE = 0x0020;
+
+        
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess,
+          long lpBaseAddress, byte[] lpBuffer, UIntPtr dwSize, ref UIntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] buffer, int size, int lpNumberOfBytesWritten);
+
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+        public static void Hack(int status, int pid, Int32 address)
+        {
+            Form1 frm1 = new Form1();
+            var processHandle = OpenProcess(0x0020FFF, false, pid);
+            int dec = status * 8;
+            byte[] bytes = BitConverter.GetBytes(dec);
+
+            for (int i = address; i < address + 64; i += 8)
+            {
+                WriteProcessMemory((int)processHandle, i, bytes, (int)bytes.Length, 0);
+            }
+        }
+    }
+}
